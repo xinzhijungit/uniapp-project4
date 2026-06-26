4e23fd307b665615f0403f31609146c168dea0e1
辛志军 <xinzhijun@chinasofti.com>
2026-06-26 17:28:57 +0800

    修复Vercel部署：让API入口文件同时提供静态文件服务，不再依赖@vercel/static

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 const { app } = require('../server/src/index')
+const path = require('path')
+const express = require('express')
+
+app.use(express.static(path.join(__dirname, '../dist/build/h5')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/build/h5/index.html'))
+})
 
 module.exports = app
