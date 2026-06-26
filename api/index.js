44986bcf461a0a48cb4e3bc1bbf92553b5e593e3
辛志军 <xinzhijun@chinasofti.com>
2026-06-26 17:36:16 +0800

    修复Vercel部署：使用@vercel/static托管静态文件，避免模块加载问题

--- api/index.js
+++ api/index.js
@@ -1,15 +1,2 @@
-const express = require('express')
-const path = require('path')
-const { app: serverApp } = require('../server/src/index')
-
-const app = express()
-
-app.use(express.static(path.join(__dirname, '../dist/build/h5')))
-
-app.use('/api', serverApp)
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/build/h5/index.html'))
-})
-
+const { app } = require('../server/src/index')
 module.exports = app
