ce9e5aa68426577096671551fea34cdfcd822448
辛志军 <xinzhijun@chinasofti.com>
2026-06-26 17:33:44 +0800

    修复Vercel部署：修复路由顺序问题，创建新Express应用正确处理静态文件和API

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-const { app } = require('../server/src/index')
-const path = require('path')
 const express = require('express')
+const path = require('path')
+const { app: serverApp } = require('../server/src/index')
+
+const app = express()
 
 app.use(express.static(path.join(__dirname, '../dist/build/h5')))
+
+app.use('/api', serverApp)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/build/h5/index.html'))
